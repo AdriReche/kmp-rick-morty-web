340c25f96d49b0162ef31ffe75e5d3adf21b0d3e
ew3w6bu <adrian.reche@volkswagen-groupservices.com>
2025-08-02 23:55:04 +0200

    Refactor UI components to improve styling and consistency

--- composeApp/src/wasmJsMain/kotlin/org/arexdev/rickmortyapp/ui/home/HomeScreen.kt
+++ composeApp/src/wasmJsMain/kotlin/org/arexdev/rickmortyapp/ui/home/HomeScreen.kt
@@ -1,29 +1,57 @@
 package org.arexdev.rickmortyapp.ui.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import org.arexdev.rickmortyapp.ui.core.BackgroundPrimaryColor
+import org.arexdev.rickmortyapp.ui.core.BackgroundSecondaryColor
+import org.arexdev.rickmortyapp.ui.core.BackgroundTertiaryColor
+import org.arexdev.rickmortyapp.ui.core.DefaultTextColor
+import org.arexdev.rickmortyapp.ui.core.Green
 import org.arexdev.rickmortyapp.ui.core.navigation.bottomnavigation.BottomBarItem
 import org.arexdev.rickmortyapp.ui.core.navigation.bottomnavigation.NavigationBottomWrapper
+import org.jetbrains.compose.resources.painterResource
+import rickmortyweb.composeapp.generated.resources.Res
+import rickmortyweb.composeapp.generated.resources.ricktoolbar
 
 @Composable
 fun HomeScreen(mainNavController: NavHostController) {
     val items = listOf(BottomBarItem.Episodes(), BottomBarItem.Characters())
     val bottomBarNavController = rememberNavController()
-    Scaffold(bottomBar = { BottomNavigationBar(items, bottomBarNavController) }) { padding ->
+    Scaffold(
+        bottomBar = { BottomNavigationBar(items, bottomBarNavController) },
+        topBar = { TopBar() }
+    ) { padding ->
         Box(modifier = Modifier.padding(padding)) {
             NavigationBottomWrapper(bottomBarNavController, mainNavController)
         }
+    }
+}
+
+@Composable
+fun TopBar() {
+    Box(modifier = Modifier.fillMaxWidth().background(BackgroundPrimaryColor), contentAlignment = Alignment.TopCenter) {
+        Image(
+            painter = painterResource(Res.drawable.ricktoolbar),
+            contentDescription = null,
+            modifier = Modifier.padding(start = 16.dp, top = 32.dp, bottom = 8.dp),
+        )
     }
 }
 
@@ -32,11 +60,16 @@ fun BottomNavigationBar(items: List<BottomBarItem>, navController: NavHostContro
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentDestination = navBackStackEntry?.destination
 
-    NavigationBar {
+    NavigationBar(containerColor = BackgroundSecondaryColor, contentColor = Green) {
         items.forEach { item ->
             NavigationBarItem(
+                colors = NavigationBarItemDefaults.colors(
+                    indicatorColor = Green,
+                    selectedIconColor = BackgroundTertiaryColor,
+                    unselectedIconColor = Green,
+                ),
                 icon = item.icon,
-                label = { Text(item.title) },
+                label = { Text(item.title, color = DefaultTextColor) },
                 alwaysShowLabel = false,
                 onClick = {
                     navController.navigate(item.route) {
--- composeApp/src/wasmJsMain/kotlin/org/arexdev/rickmortyapp/ui/home/tabs/episodes/EpisodesScreen.kt
+++ composeApp/src/wasmJsMain/kotlin/org/arexdev/rickmortyapp/ui/home/tabs/episodes/EpisodesScreen.kt
@@ -1,5 +1,6 @@
 package org.arexdev.rickmortyapp.ui.home.tabs.episodes
 
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -8,6 +9,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,6 +21,7 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -27,15 +30,21 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import kotlinx.browser.document
 import org.arexdev.rickmortyapp.di.Provider
 import org.arexdev.rickmortyapp.domain.model.EpisodeModel
 import org.arexdev.rickmortyapp.domain.model.SeasonEpisode
+import org.arexdev.rickmortyapp.ui.core.BackgroundPrimaryColor
+import org.arexdev.rickmortyapp.ui.core.DefaultTextColor
+import org.arexdev.rickmortyapp.ui.core.PlaceholderColor
 import org.arexdev.rickmortyapp.ui.core.component.HtmlView
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.painterResource
 import rickmortyweb.composeapp.generated.resources.Res
+import rickmortyweb.composeapp.generated.resources.placeholder
 import rickmortyweb.composeapp.generated.resources.portal
 import rickmortyweb.composeapp.generated.resources.season1
 import rickmortyweb.composeapp.generated.resources.season2
@@ -50,14 +59,14 @@ fun EpisodesScreen() {
     val episodesViewModel = remember { Provider.episodesViewModel }
     val state by episodesViewModel.state.collectAsState()
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize().background(BackgroundPrimaryColor),
         verticalArrangement = Arrangement.spacedBy(12.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Spacer(modifier = Modifier.height(16.dp))
         EpisodesScreenPag(episodesViewModel)
         HideVideoButton(playVideo = state.playVideo, onCloseVideo = { episodesViewModel.onCloseVideo() })
         EpisodePlayer(playVideo = state.playVideo)
-
     }
 }
 
@@ -82,25 +91,43 @@ fun HideVideoButton(playVideo: String, onCloseVideo: () -> Unit) {
 
 @Composable
 fun EpisodePlayer(playVideo: String) {
-    AnimatedVisibility(playVideo.isNotBlank()) {
-        ElevatedCard(
-            modifier = Modifier.fillMaxWidth(0.75f).fillMaxHeight(0.75f).padding(16.dp)
-                .border(3.dp, Color.Green, CardDefaults.elevatedShape)
-        ) {
-            Box(modifier = Modifier.background(Color.Black).fillMaxSize()) {
-                Box(contentAlignment = Alignment.Center, modifier = Modifier.padding(16.dp)) {
-                    HtmlView(
-                        modifier = Modifier.fillMaxSize(),
-                        factory = {
-                            val video = document.createElement("video")
-                            video.setAttribute("controls", "true")
-                            video.setAttribute("autoplay", "true")
-                            video.setAttribute(
-                                "src",
-                                playVideo
-                            )
-                            video
-                        }
+    AnimatedContent(playVideo.isNotBlank()) { condition ->
+        if (condition) {
+            ElevatedCard(
+                modifier = Modifier.fillMaxWidth(0.75f).fillMaxHeight(0.75f).padding(16.dp)
+                    .border(3.dp, Color.Green, CardDefaults.elevatedShape)
+            ) {
+                Box(modifier = Modifier.background(Color.Black).fillMaxSize()) {
+                    Box(contentAlignment = Alignment.Center, modifier = Modifier.padding(16.dp)) {
+                        HtmlView(
+                            modifier = Modifier.fillMaxSize(),
+                            factory = {
+                                val video = document.createElement("video")
+                                video.setAttribute("controls", "true")
+                                video.setAttribute("autoplay", "true")
+                                video.setAttribute(
+                                    "src",
+                                    playVideo
+                                )
+                                video
+                            }
+                        )
+                    }
+                }
+            }
+        } else {
+            ElevatedCard(
+                modifier = Modifier.fillMaxWidth().padding(16.dp),
+                colors = CardDefaults.elevatedCardColors(containerColor = PlaceholderColor)
+            ) {
+                Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                    Image(painter = painterResource(Res.drawable.placeholder), contentDescription = null)
+                    Spacer(Modifier.height(4.dp))
+                    Text(
+                        "Aw, jeez, you gotta click the video, guys! I mean, it might be important or something!",
+                        color = DefaultTextColor,
+                        fontStyle = FontStyle.Italic,
+                        modifier = Modifier.padding(16.dp)
                     )
                 }
             }
@@ -144,6 +171,8 @@ fun EpisodeItemList(episodeModel: EpisodeModel, onEpisodeSelected: (String) -> U
             contentScale = ContentScale.Crop,
             painter = painterResource(getSeasonImage(episodeModel.season)),
         )
+        Spacer(modifier = Modifier.height(2.dp))
+        Text(text = episodeModel.episode, color = DefaultTextColor, fontWeight = FontWeight.Bold)
     }
 }
 
